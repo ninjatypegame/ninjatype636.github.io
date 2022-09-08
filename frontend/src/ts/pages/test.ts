7f489e9af7c54ee4d60dde286449766f65a048c7
abir636 <111847765+abir636@users.noreply.github.com>
2022-09-08 13:17:15 +0600

    Add files via upload

---
+++ frontend/src/ts/pages/test.ts
@@ -0,0 +1,34 @@
+import Config from "../config";
+import * as TestStats from "../test/test-stats";
+import * as TestUI from "../test/test-ui";
+import * as ManualRestart from "../test/manual-restart-tracker";
+import * as TestLogic from "../test/test-logic";
+import * as Funbox from "../test/funbox";
+import Page from "./page";
+import { updateTestPageAds } from "../controllers/ad-controller";
+
+export const page = new Page(
+  "test",
+  $(".page.pageTest"),
+  "/",
+  async () => {
+    TestLogic.restart();
+    Funbox.clear();
+    $("#wordsInput").focusout();
+  },
+  async () => {
+    updateTestPageAds(true);
+  },
+  async () => {
+    updateTestPageAds(false);
+    TestStats.resetIncomplete();
+    ManualRestart.set();
+    TestLogic.restart({
+      noAnim: true,
+    });
+    Funbox.activate(Config.funbox);
+  },
+  async () => {
+    TestUI.focusWords();
+  }
+);
